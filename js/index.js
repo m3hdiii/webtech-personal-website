b69e799b74fd6067810ccad0a38bacf4ee608461
ix56@yahoo.com <D321Mo321!#^%>
2017-02-05 07:27:46 +0800

    initial contents ~

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Mehdi on 1/29/2017.
+ */
+
+// (function () {
+//     //FIXME do some new fixes
+//     alert('Hello');
+// })();
+//
+//
+
+function makeTimes7Function() {
+    var m = 7;
+    return (function (n) {
+        return m * n;
+    });
+    
+}
