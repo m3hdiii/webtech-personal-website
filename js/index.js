0b917865396c4b041062131cbd1744ebc395ba13
ix56@yahoo.com <D321Mo321!#^%>
2017-02-05 23:29:53 +0800

    javascript file got empty ~

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
 /**
  * Created by Mehdi on 1/29/2017.
  */
-
-// (function () {
-//     //FIXME do some new fixes
-//     alert('Hello');
-// })();
-//
-//
-
-function makeTimes7Function() {
-    var m = 7;
-    return (function (n) {
-        return m * n;
-    });
-    
-}
